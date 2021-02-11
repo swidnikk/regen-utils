36ee8a4145218dfbfa0e05dafb596d25ac78d929
patogit <interpueblo@riseup.net>
2021-02-10 21:22:09 -0500

    make user relative with user instead of ubuntu

--- install-regen-logrotate.sh
+++ install-regen-logrotate.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-LOG_PATH="/home/ubuntu/logs/regen"
+LOG_PATH="/home/$USER/logs/regen"
 LOG_FILE=$LOG_PATH/regen-utils.log
+
+mkdir -p $LOG_PATH
 
 exec &> >(tee -a "$LOG_FILE")
 
 echo [$(date -u)]
 echo ... Installing Regen Log Rotation and Crontab
-sudo cp /home/ubuntu/code/regen-utils/regen.logrotate /etc/logrotate.d/regen
-sudo bash -c "echo '* * * * * * ubuntu sudo logrotate -f /etc/logrotate.d/regen' > /etc/cron.d/regen"
 
-echo Log rotation will run every minute
+echo "/home/$USER/logs/regen/*.log {
+    su $USER $USER
+    hourly
+    create 0644 $USER $USER
+    rotate 5
+    dateext
+    dateformat -%Y%m%d%H-%s
+    size=1M
+    notifempty
+    copytruncate
+    postrotate
+	echo [$(date -u)] >> /home/$USER/logs/regen/regen-utils.log
+	echo "Regen log $1 rotated" >> /home/$USER/logs/regen/regen-utils.log
+    endscript
+}
+" >regen.logrotate
+
+
+sudo mv regen.logrotate /etc/logrotate.d/regen
+sudo bash -c "echo '* * * * * * $USER sudo logrotate -f /etc/logrotate.d/regen' > /etc/cron.d/regen"
+
+echo Log rotation will run every minute.
+echo Logs are in /home/$USER/logs/regen.
 echo Done.
